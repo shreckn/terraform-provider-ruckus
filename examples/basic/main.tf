eb97b313626d1b56e12666f1151d7f91411784c8
Nathan Shreck <shreckn@msoe.edu>
2026-03-13 14:04:42 -0500

    Update examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -20,9 +20,30 @@ data "ruckus_zone" "hq" {
   name = "HQ"
 }
 
-resource "ruckus_wlan" "guest" {
+resource "ruckus_wlan" "corp" {
   zone_id     = data.ruckus_zone.hq.id
-  name        = "Guest WiFi"
-  ssid        = "Guest-WLAN"
-  description = "Guest access"
+  name        = "Corp-Staff"
+  ssid        = "Corp-Staff"
+  description = "Corp secure WLAN"
+
+  security {
+    mode        = "wpa2_psk"
+    passphrase  = var.corp_psk
+    encryption  = "ccmp"
+  }
+
+  vlan {
+    access_vlan  = 120
+    dynamic_vlan = false
+  }
+
+  radio {
+    band             = "5"
+    client_isolation = true
+  }
+
+  advanced {
+    min_bss_rate = 6000
+    ofdma        = true
+  }
 }
